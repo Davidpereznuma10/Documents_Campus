b9033da47a5b674127c344ab53af85720f99cc9d
David Pérez Numa <jdpereznuma@gmail.com>
2023-03-08 15:42:07 -0500

    feat: :ambulance: Add Method Replace
    we use this method when we want to change de a word but this funtion just change the first one that it meet.

--- main.js
+++ main.js
@@ -1,5 +1,4 @@
-const Metodo_includes= ("this its a example of method includes");
+const Metodo_Replace= ("this its a example of method replace, and this is so usefull when you just wanna take one piece of a text");
 
-console.log(Metodo_includes.includes("t","m"));
-console.log(Metodo_includes.includes("Meet"));
-console.log(Metodo_includes.includes("includes"))
+console.log(Metodo_Replace);
+console.log(Metodo_Replace.replace("example","other point"));
