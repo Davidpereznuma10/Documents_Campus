4f0c926b1b4888508d76d3b708fe1ed798939bf5
David Pérez Numa <jdpereznuma@gmail.com>
2023-03-08 16:21:50 -0500

    feat: :ambulance: Add method split
    this method beacame our string in array, for that reason it is separe by the parameter that we idicated it., in this case was  a space.

--- main.js
+++ main.js
@@ -1,6 +1,4 @@
-const Method_trim= (                                     "Batman found a new ally in a cop named John Blake, who'd also deduced the Dark Knight's secret identity as Bruce Wayne. This Batman quote lines up with the movie's focus on legacy, as Bruce Wayne began preparing John to potentially take over as the new Dark Knight after he retired.                                  ");
+const Method_split= ("Batman found a new ally in a cop named John Blake, who'd also deduced the Dark Knight's secret identity as Bruce Wayne. This Batman quote lines up with the movie's focus on legacy, as Bruce Wayne began preparing John to potentially take over as the new Dark Knight after he retired.");
+let divicion=Method_split.split(' ');
 
-console,log(Method_trim);
-console.log(Method_trim.trim());
-console.log(Method_trim.trimStart());
-console.log(Method_trim.trimEnd());
+console,log(divicion);
