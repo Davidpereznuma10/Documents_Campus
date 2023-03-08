5c52ba92f87ee15dd1061b6ecda2f17ee314e00a
David Pérez Numa <jdpereznuma@gmail.com>
2023-03-08 15:47:47 -0500

    feat: :ambulance: Add Method charAt
    This method  is used to know the position of specific letter, in case that we wanna know the position of a latest letter we need use legth and menus before de number to specific the letter.

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-const Metodo_repeat= ("La repeticion de la repetidera");
+const Metodo_charAt= ("La repeticion de la repetidera");
 
-console.log(Metodo_repeat.repeat(1));
-console.log(Metodo_repeat.repeat(3));
+console.log(Metodo_charAt.charAt(Metodo_charAt.length-1));
+console.log(Metodo_charAt.charAt(0));
