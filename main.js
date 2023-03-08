b2abee8792143dc95a81fe5a993d9d2e9c5c3112
David Pérez Numa <jdpereznuma@gmail.com>
2023-03-08 16:27:44 -0500

    feat: :ambulance: Add method concat
    How we can see in our console each one of our string are separate, but when we concat  to became all them in just one string.

--- main.js
+++ main.js
@@ -1,4 +1,5 @@
-const Method_split= ("Batman found a new ally in a cop named John Blake, who'd also deduced the Dark Knight's secret identity as Bruce Wayne. This Batman quote lines up with the movie's focus on legacy, as Bruce Wayne began preparing John to potentially take over as the new Dark Knight after he retired.");
-let divicion=Method_split.split(' ');
+const strej= ("Hello awasome world");
+let divicion = strej.split(' ');
 
-console,log(divicion);
+console.log(strej);
+console.log(divicion);
