a002b82fea760a06a85f98254b85085a9eb59637
David Pérez Numa <jdpereznuma@gmail.com>
2023-03-08 15:59:49 -0500

    feat: :ambulance: Add method charCodeAt
    The charCodeAt() method is used to retrieve the Unicode of the character at a specified index (position) in a string [1]. It takes one parameter, which is the index of the character you want to retrieve the Unicode of. For example, the following code will return the Unicode of the first character in the string..

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-const Metodo_charAt= ("La repeticion de la repetidera");
+const Metodo_CharCodeAt= ("La h repeticion de la repetidera");
 
-console.log(Metodo_charAt.charAt(Metodo_charAt.length-1));
-console.log(Metodo_charAt.charAt(0));
+console.log(Metodo_CharCodeAt.charCodeAt(1));
+console.log(Metodo_CharCodeAt.charCodeAt(4));
