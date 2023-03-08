fc82012b01ada892b0be23f5185c3d8012f259a0
David Pérez Numa <jdpereznuma@gmail.com>
2023-03-08 16:10:14 -0500

    feat: :ambulance: Add method indexOf
    we can use this method when we wanna know the especific position of a letter o word.

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-const Metodo_CharCodeAt= ("La h repeticion de la repetidera");
+const Method_CodePointAt= ("La h repeticion de la repetidera");
 
-console.log(Metodo_CharCodeAt.charCodeAt(1));
-console.log(Metodo_CharCodeAt.charCodeAt(4));
+console.log(Method_CodePointAt.codePointAt(1));
+console.log(Method_CodePointAt.codePointAt(4));
