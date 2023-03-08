e3eeb1048d01cbb3702c79195d72844ed37db989
David Pérez Numa <jdpereznuma@gmail.com>
2023-03-08 15:46:13 -0500

    feat: :ambulance: Add method Substring
    This method make the same thing that slice, but this return like a subset.

--- main.js
+++ main.js
@@ -1,5 +1,6 @@
-const Metodo_Slice= ("Michael Keaton (1989-1992, 2023) To the original Batman of the big screen goes the crown. Unflinching, un-phased, unyielding, understated, unpretentious, and numerous other un- words, Michael Keaton's Batman really is the silent warrior at the gates.\nBatman found a new ally in a cop named John Blake, who'd also deduced the Dark Knight's secret identity as Bruce Wayne. This Batman quote lines up with the movie's focus on legacy, as Bruce Wayne began preparing John to potentially take over as the new Dark Knight after he retired.");
-let nuevoString=Metodo_Slice.slice("251","515")
+const Metodo_substring= ("Michael Keaton (1989-1992, 2023) To the original Batman of the big screen goes the crown. Unflinching, un-phased, unyielding, understated, unpretentious, and numerous other un- words, Michael Keaton's Batman really is the silent warrior at the gates.\nBatman found a new ally in a cop named John Blake, who'd also deduced the Dark Knight's secret identity as Bruce Wayne. This Batman quote lines up with the movie's focus on legacy, as Bruce Wayne began preparing John to potentially take over as the new Dark Knight after he retired.");
+let nuevoString=Metodo_substring.substring("251","515")
 
-console.log(Metodo_Slice.length);
+
+console.log(Metodo_substring.length);
 console.log(nuevoString)
