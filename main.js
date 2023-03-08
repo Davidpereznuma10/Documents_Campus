7dc760ea2255a6ad4b536ef806b1eef7ac59a314
David Pérez Numa <jdpereznuma@gmail.com>
2023-03-08 16:21:02 -0500

    feat: :ambulance: Add method trim
    this method is used to delete de space in the beginning or end of our text we can use a specific comand to remove on the  beginning with "trimStart" or  end with "trimEnd" or just "trim" to both

--- main.js
+++ main.js
@@ -1,4 +1,6 @@
-const Method_toLowerCase= ("LA REPETICION DE LA REPETIDERA EN MAYUSCULA QUE CAMBIA CUANDO USAMOS toLowerCase");
+const Method_trim= (                                     "Batman found a new ally in a cop named John Blake, who'd also deduced the Dark Knight's secret identity as Bruce Wayne. This Batman quote lines up with the movie's focus on legacy, as Bruce Wayne began preparing John to potentially take over as the new Dark Knight after he retired.                                  ");
 
-console.log(Method_toLowerCase);
-console.log(Method_toLowerCase.toLowerCase());
+console,log(Method_trim);
+console.log(Method_trim.trim());
+console.log(Method_trim.trimStart());
+console.log(Method_trim.trimEnd());
