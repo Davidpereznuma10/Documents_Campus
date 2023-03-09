aef791bab1eba8d6653d16bed3ce157862d6959b
David Pérez <jdpereznuma@gmail.com>
2023-03-09 17:00:01 -0500

    feat: :sparkles: Add Modulos operator
    This operator is represented by the % character and is used to get the remainder of a division operation between two operands. The remainder left over when one operand is divided by a second operand can be obtained using this operator.

--- README.md
+++ README.md
@@ -1,4 +1,1 @@
-This branch is to thirt activity about types of operators
-1.aricmatic.
-2.comparison 
-3.logical
+this is the arithmetic operators
--- main.js
+++ main.js
@@ -1,5 +1,5 @@
-// Division ("/")
-let num1=10;
-let num2=2;
-let Resultado=num1/num2;
-console.log(`the result of division is: ${Resultado}`)
+// Modulus ("%")
+let num1=15164161;
+let num2=20;
+let Resultado=num1%num2;
+console.log(`The remainder of the operation is: ${Resultado}`)
