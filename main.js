d7dc80c04b6ed44de6b9123123500d9ead92b43f
David Pérez <jdpereznuma@gmail.com>
2023-03-09 17:43:26 -0500

    feat: :sparkles: Add Math.max operator
    This operator is not a built-in operator. However, there is a Math.max() method that is used to return the largest value from the numbers provided as parameters.

--- main.js
+++ main.js
@@ -1,7 +1,8 @@
-// Exponentiation  ("**")
+// Max  ("Math.max")
 //Example 1
-let expo1=9 ** 3;
-console.log(`${expo1}`)
+const myArray=Math.max(90,64,23,65,654,76,95,34,85,99,48);
+console.log(myArray);
 //Example 2
-let expo2= x **= 10;
-console.log(`${expo2}`)
+const myArray2=[90,64,23,65,654,76,95,34,85,99,48];
+const newArray = Math.max(...myArray2);
+console.log(newArray);
