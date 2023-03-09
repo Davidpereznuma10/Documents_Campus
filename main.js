04a74c80c7439387e04f37766d0730d4dd194909
David Pérez <jdpereznuma@gmail.com>
2023-03-09 16:43:54 -0500

    feat: :sparkles: feat: ambulance Add Addition operator
    the addition operator (+) is a binary operator that is used to perform arithmetic addition or string concatenation, depending on the types of the operands. When used with two numbers, it adds them together to produce a numeric result. When used with a string and a non-string value, it performs implicit type coercion and concatenates the values into a new string.

--- main.js
+++ main.js
@@ -1,5 +1,12 @@
-// Subtraction: "-"
-let num1=3;
-let num2=2;
-let Resultado=num1-num2;
-console.log(`The subtraction result is: ${Resultado}`)
+// //Addition (+)
+let num1=2;
+let num2=3;
+let Resultado=num1+num2;
+console.log(`El resultado de la suma es: ${Resultado}`)
+
+
+// //Adittion like concat
+let str1=("Hey my name is David.")
+let str2=("I am studying in Campus.")
+let add_concat=str1+" "+str2;
+console.log(add_concat)
