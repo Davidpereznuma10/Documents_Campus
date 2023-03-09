6190c336a19d09129b866421b01bb7641a9b4cca
David Pérez <jdpereznuma@gmail.com>
2023-03-09 16:51:21 -0500

    feat: :sparkles: Add Multoplication operator
    This operator is used to multiply two numbers. When used between two numbers, it returns the product of those two numbers.

--- main.js
+++ main.js
@@ -1,12 +1,5 @@
-// //Addition (+)
+// Multiplication ("*")
 let num1=2;
 let num2=3;
-let Resultado=num1+num2;
-console.log(`El resultado de la suma es: ${Resultado}`)
-
-
-// //Adittion like concat
-let str1=("Hey my name is David.")
-let str2=("I am studying in Campus.")
-let add_concat=str1+" "+str2;
-console.log(add_concat)
+let Resultado=num1*num2;
+console.log(`El resultado de la multiplicacion es: ${Resultado}`)
