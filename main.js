235524036deebfe511ded20883f25fce7af3dc83
David Pérez <jdpereznuma@gmail.com>
2023-03-09 22:13:17 -0500

    feat: :sparkles: Add Math.min operator
    This oprator is used to find the minimum value among a set of numbers. It can take any number of arguments as input and returns the smallest of those arguments. If there are no arguments or all arguments are NaN, it returns Infinity.

--- main.js
+++ main.js
@@ -1,8 +1,8 @@
-// Max  ("Math.max")
+// Min  ("Math.min")
 //Example 1
-const myArray=Math.max(90,64,23,65,654,76,95,34,85,99,48);
+const myArray=Math.min(90,64,23,65,654,76,95,34,85,99,48);
 console.log(myArray);
 //Example 2
 const myArray2=[90,64,23,65,654,76,95,34,85,99,48];
-const newArray = Math.max(...myArray2);
+const newArray = Math.min(...myArray2);
 console.log(newArray);
