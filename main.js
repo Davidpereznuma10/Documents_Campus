0dc07d9dca0cced0f2281711525b69fef3ab5314
David Pérez <jdpereznuma@gmail.com>
2023-03-09 22:30:32 -0500

    feat: :sparkles: Add  Pi operator
    This operator is used to represent the mathematical constant pi (π), which is the ratio of the circumference of a circle to its diameter.

--- main.js
+++ main.js
@@ -1,10 +1,10 @@
-// toFixed  (".toFixed")
+// Pi  (".Pi")
+const radius =7;
 //Example 1
-const num = 3.14159265359;
-const roundedNum = num.toFixed(2);
-console.log(roundedNum);
+
+const area = Math.PI * radius * radius;
+console.log("Area of the circle: " + area);
 
 //Example 2
-const num1 = 16.65461298432;
-const roundedNum1 = num1.toFixed(5);
-console.log(roundedNum1);
+let circumference = 2 * Math.PI * radius;
+console.log("Circumference of the circle: " + circumference);
