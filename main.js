76af558138169482718885e275ae7de3144da028
David Pérez <jdpereznuma@gmail.com>
2023-03-12 21:48:50 -0500

    feat: :sparkles: Add ToFixed operator
    This method used to convert a number to a string with a specified number of decimal places or not.

--- main.js
+++ main.js
@@ -1,7 +1,11 @@
-// Round  (".round")
+// ToFixed  (".toFixed")
 // Example 1
-let num1 = Math.round(8.279);
-console.log(num1);
+let x = 3.14159265359;
+let y = x.toFixed(2);
+let type= typeof(y)
+console.log(`This is a ${type}: ${y}`);
 // Example 2
-let num2 = Math.round(232.55);
-console.log(num2);
+let a = 3.14159265359;
+let b = x.toFixed();
+let type2= typeof(b)
+console.log(`This is a ${type2}: ${b}`);
