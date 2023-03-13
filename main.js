a95253d2a054315570c691e2077c6cc38b930d67
David Pérez <jdpereznuma@gmail.com>
2023-03-12 22:01:12 -0500

    feat: :sparkles: This is the most basic assignment operator Assigns the value of the expression on the right side to the variable on the left side.

--- main.js
+++ main.js
@@ -1,11 +1,7 @@
-// ToFixed  (".toFixed")
-// Example 1
-let x = 3.14159265359;
-let y = x.toFixed(2);
-let type= typeof(y)
-console.log(`This is a ${type}: ${y}`);
-// Example 2
-let a = 3.14159265359;
-let b = x.toFixed();
-let type2= typeof(b)
-console.log(`This is a ${type2}: ${b}`);
+// "="  ("=")
+let value="Hi";
+var value2=23;
+const value3=true;
+
+console.log(`Add valor to:\n${value}\n${value2}\n${value3}`);
+
