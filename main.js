39aa824e93e7346b8965cd61929fcb802d2dafc1
David Pérez <jdpereznuma@gmail.com>
2023-03-13 03:58:14 -0500

    feat: :sparkles: Add ">" Operator
    This operator is used as a comparison operator to compare two values be the left value bigger than right value.

--- main.js
+++ main.js
@@ -1,26 +1,21 @@
-// "!=="  ("!==")
+// ">"  (">")
 //Example 1
 let a = 5;
-let b = '5';
-if (a !== b) {
-    console.log('a and b are equal');
-} else {
-    console.log('a and b are not equal');
-}
+let b = 5;
+console.log(a > b);
 //Example 2
 let x = 5;
 let y = 6;
-if (x !== y) {
-    console.log('x and y are equal');
-} else {
-    console.log('x and y are not equal');
-}
+    console.log(x > y);
 //Example 3
-let d = 58;
-let c = 58;
-if (d !== c) {
-    console.log('d and c are equal');
-} else {
-    console.log('d and c are not equal');
-}
+let d = 8;
+let c = 7;
+console.log(d > c);
+//Example 4
+let str1 = "Kiwi";
+let str2 = "banana";
+console.log(str2 > str1);
+let date1 = "1999-10-31";
+let date2 = "2023-10-31";
+console.log(date1 > date2);
 
