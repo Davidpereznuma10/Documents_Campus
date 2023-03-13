c1089cab92481da297d30fb5f36699810cc04dae
David Pérez <jdpereznuma@gmail.com>
2023-03-13 03:22:12 -0500

    feat: :sparkles: Add "/=" Operator
    Este operador divide el valor actual de la variable del lado izquierdo por el valor del lado derecho y asigna el resultado a la variable del lado izquierdo.

--- main.js
+++ main.js
@@ -1,10 +1,10 @@
-// "*="  ("*=")
+// "/="  ("/=")
 //Example 1
-let x= 3;
-x *= 2;
+let x= 6;
+x /= 2;
 console.log(`${x}`);
 //Example 2
-let y= 8;
-y *= 2;
+let y= 240;
+y /= 8;
 console.log(`${y}`);
 
