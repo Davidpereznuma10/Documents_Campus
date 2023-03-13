a7a17ebbfce84815fc394507d97aa9d08cb6ef55
David Pérez <jdpereznuma@gmail.com>
2023-03-13 03:47:41 -0500

    feat: :sparkles: Add "===" Operator
    Strict equality checks that the operands are equal in both value and type

--- main.js
+++ main.js
@@ -1,18 +1,26 @@
-// "!="  ("!=")
+// "==="  ("===")
 //Example 1
 let a = 5;
 let b = '5';
-if (a != b) {
+if (a === b) {
     console.log('a and b are equal');
 } else {
-    console.log('a and b are different');
+    console.log('a and b are not equal');
 }
 //Example 2
 let x = 5;
-let y = '6';
-if (x != y) {
+let y = 6;
+if (x === y) {
     console.log('x and y are equal');
 } else {
-    console.log('x and y are different');
+    console.log('x and y are not equal');
+}
+//Example 3
+let d = 58;
+let c = 58;
+if (d === c) {
+    console.log('d and c are equal');
+} else {
+    console.log('d and c are not equal');
 }
 
