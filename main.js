b3a2a56e60b95d62551ee282601997c1728478a8
David Pérez <jdpereznuma@gmail.com>
2023-03-13 03:43:52 -0500

    feat: :sparkles: Add "!=" Operator
    This operator returns true if the two operands are not equal, after any necessary type casts.

--- main.js
+++ main.js
@@ -1,18 +1,18 @@
-// "=="  ("==")
+// "!="  ("!=")
 //Example 1
 let a = 5;
 let b = '5';
-if (a == b) {
+if (a != b) {
     console.log('a and b are equal');
 } else {
-    console.log('a and b are not equal');
+    console.log('a and b are different');
 }
 //Example 2
 let x = 5;
 let y = '6';
-if (x == y) {
+if (x != y) {
     console.log('x and y are equal');
 } else {
-    console.log('x and y are not equal');
+    console.log('x and y are different');
 }
 
