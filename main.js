d085237ba436b5ae06b68b6e48dbb1c6c2f69d57
David Pérez <jdpereznuma@gmail.com>
2023-03-13 04:03:25 -0500

    feat: :sparkles: Add "<" Operator
    This operator is used as a comparison operator to compare two values be the right value biger than left value.

--- main.js
+++ main.js
@@ -1,21 +1,21 @@
-// ">="  (">=")
+// "<"  ("<")
 //Example 1
 let a = 5;
 let b = 5;
-console.log(a >= b);
+console.log(a < b);
 //Example 2
 let x = 5;
 let y = 6;
-    console.log(x >= y);
+    console.log(x < y);
 //Example 3
 let d = 8;
 let c = 7;
-console.log(d >= c);
+console.log(d < c);
 //Example 4
 let str1 = "Kiwi";
 let str2 = "kiwi";
-console.log(str2 >= str1);
+console.log(str2 < str1);
 let date1 = "1999-10-31";
 let date2 = "2023-10-31";
-console.log(date1 >= date2);
+console.log(date1 < date2);
 
